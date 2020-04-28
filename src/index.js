df89a10a3cdd9def46c64ec7ad41d714b0893ab0
Polina Chernomaz <polina@crosscompute.com>
2020-04-28 01:51:01 -0400

    Passing data from NYC Open Data's NYPD Crime Complaints Histoic

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Source data CSV
+const DATA_URL = 'https://data.cityofnewyork.us/resource/qgea-i56i.csv';
+
+require('d3-request').csv(DATA_URL, (error, response) => {
+    if (!error) {
+      const data = response.map(d => [Number(d.longitude), Number(d.latitude)]);
+    ReactDOM.render(
+      <React.StrictMode>
+        <App data={data} />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  }
+});
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
